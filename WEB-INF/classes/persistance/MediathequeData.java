daaf53cfd64aea00715785ecac5387bca8b496aa
WhereIsYourLuck <48730787+WhereIsYourLuck@users.noreply.github.com>
2022-02-17 11:22:23 +0100

    Base Projet
    D'après le document pdf du prof

---
+++ WEB-INF/classes/persistance/MediathequeData.java
@@ -0,0 +1,35 @@
+package persistance;
+
+import java.util.List;
+
+import mediatek2022.Document;
+import mediatek2022.PersistentMediatheque;
+import mediatek2022.Utilisateur;
+
+public class MediathequeData implements PersistentMediatheque {
+
+	@Override
+	public List<Document> tousLesDocumentsDisponibles() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Document getDocument(int numDocument) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Utilisateur getUser(String login, String password) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void ajoutDocument(int type, Object... args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
