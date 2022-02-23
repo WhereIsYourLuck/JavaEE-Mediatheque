19292c4877cf86ce8c823e90803c21e437d8c9c9
WhereIsYourLuck <48730787+WhereIsYourLuck@users.noreply.github.com>
2022-02-23 15:28:04 +0100

    Base du project

--- WEB-INF/classes/init/InitListener.java
+++ WEB-INF/classes/init/InitListener.java
@@ -1,18 +1,17 @@
+package init;
+
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
 public class InitListener implements ServletContextListener {
 
-/* mettre dans le web.xml
-<listener>
-    <listener-class>InitListener</listener-class>
-</listener>
-*/
-
 	@Override
 	public void contextInitialized(ServletContextEvent sce) {
-		Class.forName("persistance.MediathequeData");
+			
 		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
 	}
 
+	@Override
+	public void contextDestroyed(ServletContextEvent arg0) {
+	}
 }
